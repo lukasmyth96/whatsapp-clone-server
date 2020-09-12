22a3430ca6b1d59dfc0a3ae247d95374d89d732c
lukasmyth96 <luka@nate.tech>
2020-09-12 20:10:04 +0100

    removed REST chats route

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import { ApolloServer, gql } from 'apollo-server-express';
 import express from 'express';
 import cors from 'cors';
 
-import { chats } from './db';
 import schema from './schema';
 
 const app = express();
@@ -11,10 +10,6 @@ app.use(express.json());
 
 app.get('/_ping', (req, res) => {
   res.send('pong');
-});
-
-app.get('/chats', (req, res) => {
-  res.json(chats);
 });
 
 const server = new ApolloServer({ schema });
